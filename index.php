7e324029930df5b5ed486719986b568793f1efc6
Eugene25 <21500766@handong.edu>
2019-03-21 15:24:18 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<section class="yapp-section p-3 p-lg-5 d-flex d-column" id="main">
+        <div class="my-auto">
+          <h1 class="mb-0">ENGLISH
+            <span class="text-primary">YAPP!</span>
+          </h1>
+          <div class="subheading mb-5">지금 당신의 단어장을 만들어보세요
+          </div>
+          <p class="lead mb-5">Lets start the new way of learning english with ENGLISH <span class="text-primary">YAPP!</span></p>
+        </div>
+      </section>
